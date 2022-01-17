1a47168fab9d24d3ab3e53686817a815a3a011b1
MarioCenteno <mario.centeno@tecnico.ulisboa.pt>
2022-01-17 23:07:40 +0000

    HUD

--- CGJApp/CGJApp/MainFile.cpp
+++ CGJApp/CGJApp/MainFile.cpp
@@ -33,7 +33,7 @@
 #include "AVTmathLib.h"
 #include "VertexAttrDef.h"
 #include "geometry.h"
-
+#include "flare.h"
 
 #include "avtFreeType.h"
 #include "Texture_Loader.h"
@@ -48,6 +48,7 @@
 #include "Camera/OrthographicCamera.h"
 #include "Lights/PointLight.h"
 #include "Lights/DirectionalLight.h"
+#include "GameHudManager.h"
 #define MAX_LIGHTS 9
 
 using namespace std;
@@ -89,18 +90,20 @@ GLint vm_uniformId;
 GLint normal_uniformId;
 GLint lPos_uniformId;
 GLint view_uniformId;
-GLint tex_loc, tex_loc1, tex_loc2;
+GLint tex_loc, tex_loc1, tex_loc2, tex_loc3;
 GLint texMode_uniformId, shadowMode_uniformId;
 GLint toggleFog_uniformId;
 int toggleFog;
 
 GLuint TextureArray[7];
 
+GLuint FlareTextureArray[5];
 //Flare effect
-//FLARE_DEF AVTflare;
+FLARE_DEF AVTflare;
 float lightScreenPos[3];  //Position of the light in Window Coordinates
 bool flareEffect;
 float flareLightPos[4] = { 20.0f, 10.0f, 0.0f, 1.0f };
+
 	
 // Camera Position
 float camX, camY, camZ;
@@ -148,6 +151,7 @@ Car car;
 MyMesh cubeMesh;
 MyMesh torusMesh;
 MyMesh sphereMesh;
+MyMesh quadMesh;
 
 MyMesh* bodyMesh;
 MyMesh* wheelsMesh;
@@ -169,6 +173,17 @@ bool flag = true;
 
 
 bool pause = false;
+bool restart = false;
+
+inline double clamp(const double x, const double min, const double max) {
+	return (x < min ? min : (x > max ? max : x));
+}
+
+inline int clampi(const int x, const int min, const int max) {
+	return (x < min ? min : (x > max ? max : x));
+}
+
+GameHudManager gameManager;
 
 void renderScene(void);
 
@@ -288,6 +303,92 @@ void SendLights(LightProperties* lProps, int lightId)
 // Render stufff
 //
 
+void render_flare(FLARE_DEF* flare, int lx, int ly, int* m_viewport) {  //lx, ly represent the projected position of light on viewport
+
+	int     dx, dy;          // Screen coordinates of "destination"
+	int     px, py;          // Screen coordinates of flare element
+	int		cx, cy;
+	float    maxflaredist, flaredist, flaremaxsize, flarescale, scaleDistance;
+	int     width, height, alpha;    // Piece parameters;
+	int     i;
+	float	diffuse[4];
+
+	GLint loc;
+
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	int screenMaxCoordX = m_viewport[0] + m_viewport[2] - 1;
+	int screenMaxCoordY = m_viewport[1] + m_viewport[3] - 1;
+
+	//viewport center
+	cx = m_viewport[0] + (int)(0.5f * (float)m_viewport[2]) - 1;
+	cy = m_viewport[1] + (int)(0.5f * (float)m_viewport[3]) - 1;
+
+	// Compute how far off-center the flare source is.
+	maxflaredist = sqrt(cx * cx + cy * cy);
+	flaredist = sqrt((lx - cx) * (lx - cx) + (ly - cy) * (ly - cy));
+	scaleDistance = (maxflaredist - flaredist) / maxflaredist;
+	flaremaxsize = (int)(m_viewport[2] * flare->fMaxSize);
+	flarescale = (int)(m_viewport[2] * flare->fScale);
+
+	// Destination is opposite side of centre from source
+	dx = clampi(cx + (cx - lx), m_viewport[0], screenMaxCoordX);
+	dy = clampi(cy + (cy - ly), m_viewport[1], screenMaxCoordY);
+
+	// Render each element. To be used Texture Unit 0
+
+	glUniform1i(texMode_uniformId, 4); // draw modulated textured particles 
+	glUniform1i(tex_loc3, 0);  //use TU 3
+
+	for (i = 0; i < flare->nPieces; ++i)
+	{
+		// Position is interpolated along line between start and destination.
+		px = (int)((1.0f - flare->element[i].fDistance) * lx + flare->element[i].fDistance * dx);
+		py = (int)((1.0f - flare->element[i].fDistance) * ly + flare->element[i].fDistance * dy);
+		px = clampi(px, m_viewport[0], screenMaxCoordX);
+		py = clampi(py, m_viewport[1], screenMaxCoordY);
+
+		// Piece size are 0 to 1; flare size is proportion of screen width; scale by flaredist/maxflaredist.
+		width = (int)(scaleDistance * flarescale * flare->element[i].fSize);
+
+		// Width gets clamped, to allows the off-axis flaresto keep a good size without letting the elements get big when centered.
+		if (width > flaremaxsize)  width = flaremaxsize;
+
+		height = (int)((float)m_viewport[3] / (float)m_viewport[2] * (float)width);
+		memcpy(diffuse, flare->element[i].matDiffuse, 4 * sizeof(float));
+		diffuse[3] *= scaleDistance;   //scale the alpha channel
+
+		if (width > 1)
+		{
+			// send the material - diffuse color modulated with texture
+			loc = glGetUniformLocation(shader.getProgramIndex(), "mat.diffuse");
+			glUniform4fv(loc, 1, diffuse);
+
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, FlareTextureArray[flare->element[i].textureId]);
+			pushMatrix(MODEL);
+			translate(MODEL, (float)(px - width * 0.0f), (float)(py - height * 0.0f), 0.0f);
+			scale(MODEL, (float)width, (float)height, 1);
+			computeDerivedMatrix(PROJ_VIEW_MODEL);
+			glUniformMatrix4fv(vm_uniformId, 1, GL_FALSE, mCompMatrix[VIEW_MODEL]);
+			glUniformMatrix4fv(pvm_uniformId, 1, GL_FALSE, mCompMatrix[PROJ_VIEW_MODEL]);
+			computeNormalMatrix3x3();
+			glUniformMatrix3fv(normal_uniformId, 1, GL_FALSE, mNormal3x3);
+
+			glBindVertexArray(quadMesh.vao);
+			glDrawElements(quadMesh.type, quadMesh.numIndexes, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+			popMatrix(MODEL);
+		}
+	}
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glDisable(GL_BLEND);
+}
+
 void RenderMesh(MyMesh* mesh)
 {
 	// send matrices to OGL
@@ -348,9 +449,7 @@ void detectCollisions() {
 			printf("car colided with object \n");
 			car.ResolveCollision(obj_a);
 			if (obj_a->GetType() == GameObject3D::TYPE::Orange) {
-				//
-				// 
-				//gameManager.DecreaseLives();
+				gameManager.DecreaseLives();
 				//c1 = c2 = c3 = false;
 			}
 		}
@@ -659,6 +758,37 @@ void renderScene(void) {
 	popMatrix(MODEL);
 	glEnable(GL_DEPTH_TEST);
 	glDisable(GL_BLEND);
+	// LENS FLARE
+
+	if (flareEffect) {
+
+		int flarePos[2];
+		int m_viewport[4];
+		glGetIntegerv(GL_VIEWPORT, m_viewport);
+
+		pushMatrix(MODEL);
+		loadIdentity(MODEL);
+		translate(MODEL, flareLightPos[0], flareLightPos[1], flareLightPos[2]);
+		computeDerivedMatrix(PROJ_VIEW_MODEL);  //pvm to be applied to lightPost. pvm is used in project function
+
+		if (!project(flareLightPos, lightScreenPos, m_viewport))
+			printf("Error in getting projected light in screen\n");  //Calculate the window Coordinates of the light position: the projected position of light on viewport
+		flarePos[0] = clampi((int)lightScreenPos[0], m_viewport[0], m_viewport[0] + m_viewport[2] - 1);
+		flarePos[1] = clampi((int)lightScreenPos[1], m_viewport[1], m_viewport[1] + m_viewport[3] - 1);
+		popMatrix(MODEL);
+
+		//viewer looking down at  negative z direction
+		pushMatrix(PROJECTION);
+		loadIdentity(PROJECTION);
+		pushMatrix(VIEW);
+		loadIdentity(VIEW);
+		ortho(m_viewport[0], m_viewport[0] + m_viewport[2] - 1, m_viewport[1], m_viewport[1] + m_viewport[3] - 1, -1, 1);
+		render_flare(&AVTflare, flarePos[0], flarePos[1], m_viewport);
+		popMatrix(PROJECTION);
+		popMatrix(VIEW);
+	}
+	if (!gameManager.UpdateHUD())
+		pause = true;
 
 	glutSwapBuffers();
 }
@@ -733,11 +863,34 @@ void processKeys(unsigned char key, int xx, int yy)
 			car.GetSpotLightLeft()->isEnabled = car.GetSpotLightLeft()->isEnabled ? 0 : 1;
 			car.GetSpotLightRight()->isEnabled = car.GetSpotLightRight()->isEnabled ? 0 : 1;
 			break;
+		case 'l':
+			flareEffect = !flareEffect;
+			break;
 		case 'n':
 			if (pause == false) {
 				dl.ToggleLight();
 			}
 			break;
+		case 's':
+			if (gameManager.GetLives() != 0) {
+				printf("pause\n");
+				if (pause == false) {
+					pause = true;
+					gameManager.TogglePause();
+				}
+				else
+				{
+					gameManager.TogglePause();
+					pause = false;
+				}
+			}
+		case 'r':
+			if (gameManager.GetLives() == 0) {
+				gameManager.RestartGame();
+				restart = true;
+				//gameManager.TogglePause();
+				pause = false;
+			}
 	}
 }
 
@@ -927,7 +1080,7 @@ void initMeshPrimitives()
 	sphereMesh = createSphere(1.0f, 10);
 	torusMesh = createTorus(0.5f, 1.0f, 10, 20);
 	//billboardMesh = createQuad(6, 6);
-	//quadMesh = createQuad(2, 2);
+	quadMesh = createQuad(2, 2);
 }
 
 void initLights()
@@ -985,6 +1138,28 @@ void initTextures()
 }
 
 
+void initFlare()
+{
+	//// create geometry and VAO of the quad for flare elements
+	//amesh = createQuad(1, 1);
+	//myMeshes.push_back(amesh);
+
+	//Flare elements textures
+	glGenTextures(5, FlareTextureArray);
+	Texture2D_Loader(FlareTextureArray, "crcl.tga", 0);
+	Texture2D_Loader(FlareTextureArray, "flar.tga", 1);
+	Texture2D_Loader(FlareTextureArray, "hxgn.tga", 2);
+	Texture2D_Loader(FlareTextureArray, "ring.tga", 3);
+	Texture2D_Loader(FlareTextureArray, "sun.tga", 4);
+
+
+	////Load flare from file
+	loadFlareFile(&AVTflare, "flare.txt");
+	flareEffect = false;
+}
+
+
+
 // ------------------------------------------------------------
 //
 // Model loading and OpenGL setup
@@ -998,6 +1173,9 @@ void init()
 	initLights();
 	initMeshPrimitives();
 	initFog();
+
+	gameManager = GameHudManager(&shaderText);
+
 
 	//float carPos[3] = { 0.0f, 0.0f, 0.0f };
 	car = Car(&cubeMesh, &torusMesh, true, 1.0f);
@@ -2028,6 +2206,63 @@ int main(int argc, char **argv) {
 
 	return(0);
 }
+
+
+unsigned int getTextureId(char* name) {
+	int i;
+
+	for (i = 0; i < NTEXTURES; ++i)
+	{
+		if (strncmp(name, flareTextureNames[i], strlen(name)) == 0)
+			return i;
+	}
+	return -1;
+}
+
+void loadFlareFile(FLARE_DEF* flare, char* filename)
+{
+	int     n = 0;
+	FILE* f;
+	char    buf[256];
+	int fields;
+
+	memset(flare, 0, sizeof(FLARE_DEF));
+
+	f = fopen(filename, "r");
+	if (f)
+	{
+		fgets(buf, sizeof(buf), f);
+		sscanf(buf, "%f %f", &flare->fScale, &flare->fMaxSize);
+
+		while (!feof(f))
+		{
+			char            name[8] = { '\0', };
+			double          dDist = 0.0, dSize = 0.0;
+			float			color[4];
+			int				id;
+
+			fgets(buf, sizeof(buf), f);
+			fields = sscanf(buf, "%4s %lf %lf ( %f %f %f %f )", name, &dDist, &dSize, &color[3], &color[0], &color[1], &color[2]);
+			if (fields == 7)
+			{
+				for (int i = 0; i < 4; ++i) color[i] = clamp(color[i] / 255.0f, 0.0f, 1.0f);
+				id = getTextureId(name);
+				if (id < 0) printf("Texture name not recognized\n");
+				else
+					flare->element[n].textureId = id;
+				flare->element[n].fDistance = (float)dDist;
+				flare->element[n].fSize = (float)dSize;
+				memcpy(flare->element[n].matDiffuse, color, 4 * sizeof(float));
+				++n;
+			}
+		}
+
+		flare->nPieces = n;
+		fclose(f);
+	}
+	else printf("Flare file opening error\n");
+}
+
 
 
 
